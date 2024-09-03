1bfcfbd299a684d4106093825776730dec1c2fb4
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2024-09-03 11:01:58 +0500

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'COUNTER APP',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have pushed the button this many times!'),
+            Text('$_counter',
+                style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple.shade200,
+          child: Icon(Icons.add),
+          onPressed: incrementCounter),
+    );
+  }
+}
